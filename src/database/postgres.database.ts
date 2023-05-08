0d1b2ccdbee16118a7fc3d8c27b6552c13376b6e
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2023-05-08 22:19:45 +0530

    Add database connection pool

---
+++ src/database/postgres.database.ts
@@ -0,0 +1,11 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
+    password: process.env.DB_PASSWORD
+});
+
+export default pool;
