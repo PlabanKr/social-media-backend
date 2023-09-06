71dac2d11945aa6b803a3de8a0079c3e005e34aa
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2023-09-06 11:54:43 +0530

    feat: implement upload post route with media

--- src/router/v1/post/api.post.ts
+++ src/router/v1/post/api.post.ts
@@ -1,14 +1,35 @@
 // Library Imports
 import { Request, Response, Router } from "express";
 import { QueryResult } from "pg";
+import multer from "multer";
+import path from "path";
 
 // Middleware Imports
 import { verifyToken } from "../../../middleware/auth.middleware";
 
 // Model and DB Pool Imports
 import pool from "../../../database/postgres.database";
+import { RequestWithUser } from "../../../types/auth.type";
 
 const router = Router();
+
+// Multer Storage and Update config
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        
+        const today = new Date();
+        // .replace(/:/g, '-') is to replace all colons with dashes because windows doesn't allow colons in file names
+        const fileName = "IMG " + today.toUTCString().replace(/:/g, '-') + path.extname(file.originalname);
+        cb(null, fileName);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+
 
 /* --- GET ALL POSTS --- */
 router.get('/', async (req: Request, res: Response) => {
@@ -35,6 +56,39 @@ router.get('/:id', verifyToken, async (req: Request, res: Response) => {
             }
             res.status(200).json(results.rows);
         });
+    } catch (error) {
+        console.log('Error: ', error);
+        res.status(500).send('Internal Server Error\n' + error);
+    }
+});
+
+/* --- CREATE NEW POST WITH IMAGE --- */
+router.post('/media', verifyToken, upload.single('image'), async (req: RequestWithUser, res: Response) => {
+    try {
+        const { title, content } = req.body;
+        const image = req.file?.path;
+        pool.query('SELECT uid FROM users WHERE email = $1', [req.user], (error: Error, results: QueryResult<any>) => {
+            if(error) {
+                throw error;
+            }
+            const author = results.rows[0].uid;
+            pool.query('INSERT INTO posts (post_title, post_body, author_id, has_media, media, like_count) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+            [
+                title || null,
+                content || null,
+                author || null,
+                true,
+                image || null,
+                0
+            ],
+            (error: Error, results: QueryResult<any>) => {
+                if(error) {
+                    throw error;
+                }
+                res.status(201).json(results.rows[0]);
+            });
+        });
+        // res.status(201).json({image: image, usr: req.user});
     } catch (error) {
         console.log('Error: ', error);
         res.status(500).send('Internal Server Error\n' + error);
