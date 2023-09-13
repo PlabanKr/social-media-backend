513df7a4564afa18d7b57fd8ed6523991991ac10
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2023-09-14 02:35:12 +0530

    feat: add comment routes

--- src/router/v1/api.v1.ts
+++ src/router/v1/api.v1.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import userView from "./user/api.user";
 import postView from "./post/api.post";
+import commentView from "./comment/api.comment";
 
 const router = Router();
 
 router.use('/user', userView);
 router.use('/post', postView);
+router.use('/comment', commentView);
 
 export default router;
