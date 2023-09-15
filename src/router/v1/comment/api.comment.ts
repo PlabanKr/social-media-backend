4fc46f12e61b2faf088ba112f46f83241efd357a
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2023-09-15 12:07:35 +0530

    feats: add create comment route

--- src/router/v1/comment/api.comment.ts
+++ src/router/v1/comment/api.comment.ts
@@ -7,7 +7,7 @@ import { verifyToken } from "../../../middleware/auth.middleware";
 
 // Model and DB Pool Imports
 import pool from "../../../database/postgres.database";
-// import { RequestWithUser } from "../../../types/auth.type";
+import { RequestWithUser } from "../../../types/auth.type";
 
 const router = Router();
 
@@ -35,6 +35,34 @@ router.get('/:id', verifyToken, async (req: Request, res: Response) => {
                 throw error;
             }
             res.status(200).json(results.rows);
+        });
+    } catch (error) {
+        console.log('Error: ', error);
+        res.status(500).send('Internal Server Error\n' + error);
+    }
+});
+
+/* --- CREATE COMMENT --- */
+router.post('/', verifyToken, async (req: RequestWithUser, res: Response) => {
+    try {
+        const {post_id, body} = req.body;
+        pool.query("SELECT uid FROM users WHERE email = $1", [req.user], (error: Error, results: QueryResult<any>) => {
+            if(error) {
+                throw error;
+            }
+            const author_id = results.rows[0].uid;
+            pool.query("INSERT INTO post_comments (author_id, post_id, body) VALUES ($1, $2, $3)", 
+            [
+                author_id, 
+                post_id, 
+                body
+            ], 
+            (error: Error, results: QueryResult<any>) => {
+                if(error) {
+                    throw error;
+                }
+                res.status(201).json(results.rows);
+            });
         });
     } catch (error) {
         console.log('Error: ', error);
